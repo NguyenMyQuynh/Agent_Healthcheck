16fc94c5c38010ec77ee9dbab50477ac234f16d7
NguyenMyQuynh <62002485+NguyenMyQuynh@users.noreply.github.com>
2022-09-27 22:51:51 +0700

    Update health_check_agent_group_by_policy.py

--- health_check_agent_group_by_policy.py
+++ health_check_agent_group_by_policy.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
 
 #Ver 1.2
-
 import config
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-import telegram
+import shlex
+import subprocess
 import json
 import requests
-import threading
+import telegram 
 
 from datetime import datetime
 from thehive4py.api import TheHiveApi 
 from thehive4py.models import Alert
-
-def rename(old_dict,old_name,new_name):
-    new_dict = {}
-    for key,value in zip(old_dict.keys(),old_dict.values()):
-        new_key = key if key != old_name else new_name
-        new_dict[new_key] = old_dict[key]
-    return new_dict
 
 # Get agent's status
 def AgentStatus(FLEET_URL, FLEET_PORT, FLEET_USERNAME, FLEET_PASSWORD):    
@@ -42,112 +34,188 @@ def OfflineAgentInfo(FLEET_URL, FLEET_PORT, FLEET_USERNAME, FLEET_PASSWORD):
     auth = (FLEET_USERNAME, FLEET_PASSWORD)
 
     if FLEET_URL == "hsoc.vn/monitor":
-        request = requests.get(f"https://{FLEET_URL}/api/fleet/agents", auth=auth, verify=False)
+        request = requests.get(f"https://{FLEET_URL}/api/fleet/agents?page=1&perPage=5000", auth=auth, verify=False)
     else:
-        request = requests.get(f"https://{FLEET_URL}:{FLEET_PORT}/api/fleet/agents", auth=auth, verify=False)
+        request = requests.get(f"https://{FLEET_URL}:{FLEET_PORT}/api/fleet/agents?page=1&perPage=5000", auth=auth, verify=False)
     result = json.loads(request.text)
 
+    # number_offline=0
     offline_agents = []
-    agents_offline_by_policy = {}
-    number_offline = 0
-
     for agent in result['list']:
         status = agent['status']
-        # if status == 'offline':
         if status != 'healthy' and status != 'online':
+            # number_offline +=1
             hostname = agent['local_metadata']['host']['hostname']
             ip = agent['local_metadata']['host']['ip']
             os = agent['local_metadata']['os']['full']
+            policy_id = agent['policy_id']   
             status = status
-            last_checkin = agent['last_checkin']
-            offline_agents.append((hostname, ip, os, status, last_checkin))
-            policy_id = agent["policy_id"]
-
-            if policy_id not in agents_offline_by_policy:
-                agents_offline_by_policy[policy_id] = []
-
-            agents_offline_by_policy[policy_id].append((hostname, ip, os, status, last_checkin))
-            
             try:
                 last_checkin = agent['last_checkin']
             except:
                 last_checkin = "NoCheckin"
+            offline_agents.append((hostname, ip, os, status, last_checkin, policy_id )) 
+            # if (number_offline >= 3):
+            #     break
 
-            number_offline += 1
-            if (number_offline >= 3):
-                break
+    return offline_agents
 
+# Get offline agent's info
+def OnlineAgentInfo(FLEET_URL, FLEET_PORT, FLEET_USERNAME, FLEET_PASSWORD):
+    auth = (FLEET_USERNAME, FLEET_PASSWORD)
 
     if FLEET_URL == "hsoc.vn/monitor":
-        request = requests.get(f"https://{FLEET_URL}/api/fleet/agent_policies", auth=auth, verify=False)
+        request = requests.get(f"https://{FLEET_URL}/api/fleet/agents?page=1&perPage=5000", auth=auth, verify=False)
     else:
-        request = requests.get(f"https://{FLEET_URL}:{FLEET_PORT}/api/fleet/agent_policies", auth=auth, verify=False)
+        request = requests.get(f"https://{FLEET_URL}:{FLEET_PORT}/api/fleet/agents?page=1&perPage=5000", auth=auth, verify=False)
     result = json.loads(request.text)
+  
+    # number_online=0
+    online_agents = []
+    for agent in result['list']:
+        status = agent['status']
+        if status == 'online':
+            # number_online +=1
+            hostname = agent['local_metadata']['host']['hostname']
+            ip = agent['local_metadata']['host']['ip']
+            os = agent['local_metadata']['os']['full']
+            policy_id = agent['policy_id']   
+            status = status
+            try:
+                last_checkin = agent['last_checkin']
+            except:
+                last_checkin = "NoCheckin"
+            online_agents.append((hostname, ip, os, status, last_checkin, policy_id )) 
+            # if (number_online >= 3):
+            #     break
 
-    for id in agents_offline_by_policy.keys():
-        for item in result["items"]:
-            if id == item["id"]:
-                agents_offline_by_policy = rename(agents_offline_by_policy, id, item["name"])
-    print(agents_offline_by_policy)
-    
-    return agents_offline_by_policy
+    return online_agents
 
 def SendAlertToTheHive(THEHIVE_SERVER, THEHIVE_PORT, THEHIVE_TOKEN, alert):
-    alert = alert.replace("\n", "\n\n")
+    alert = alert.replace("\n", "\n\n\n ")
+    alert = alert.replace(">>>> ", "\>>>>")
 
     api = TheHiveApi(f"http://{THEHIVE_SERVER}:{THEHIVE_PORT}", THEHIVE_TOKEN) 
     alert = Alert(
-        title='Anomal hostname', 
+        title='Anomal Loss Dataset', 
         description=alert, 
         type='external', 
         source=f'instance-{str(datetime.now())}', 
         sourceRef='alert-ref',
     ) 
    
-    api.create_alert(alert) 
+    api.create_alert(alert)
 
 def SendAlertToTelegram(TELEGRAM_TOKEN, TELEGRAM_ID, alert):
     telegram_alert = telegram.Bot(TELEGRAM_TOKEN)
     telegram_alert.send_message(chat_id=TELEGRAM_ID, text=alert)
 
+def change(FLEET_URL, FLEET_PORT, FLEET_USERNAME, FLEET_PASSWORD, id):
+    auth = (FLEET_USERNAME, FLEET_PASSWORD)
+
+    if FLEET_URL == "hsoc.vn/monitor":
+        request = requests.get(f"https://{FLEET_URL}/api/fleet/agent_policies", auth=auth, verify=False)
+    else:
+        request = requests.get(f"https://{FLEET_URL}:{FLEET_PORT}/api/fleet/agent_policies", auth=auth, verify=False)
+    result = json.loads(request.text)
+    
+    for item in result["items"]:
+        if item["id"]==id:
+            return item["name"]
+
+def GetAgentOnlineSeperateByPolicy(online_agents):
+    listAgentOnlineSeperateByPolicy={}
+    for i, agent in enumerate(online_agents):
+        hostname, ips, os, status, last_checkin, policy_id = agent
+        ip_list = []
+        for ip in ips:
+            if ip[0:2] == "10" or ip[0:6] == "172.16" or ip[0:7] == "192.168":
+                ip_list.append(ip)
+                break
+        info_agent = ([hostname, status, last_checkin, policy_id])
+        if policy_id not in listAgentOnlineSeperateByPolicy:
+            listAgentOnlineSeperateByPolicy[policy_id] = []
+        if info_agent not in listAgentOnlineSeperateByPolicy[policy_id]:
+            listAgentOnlineSeperateByPolicy[policy_id].append(info_agent)        
+    return listAgentOnlineSeperateByPolicy       
+        
+def GetAgentOfflineSeperateByPolicy(offline_agents):
+    listAgentOfflineSeperateByPolicy={}
+    number_offline=0
+    for i, agent in enumerate(offline_agents):
+        hostname, ips, os, status, last_checkin, policy_id = agent
+        ip_list = []
+        for ip in ips:
+            if ip[0:2] == "10" or ip[0:6] == "172.16" or ip[0:7] == "192.168":
+                ip_list.append(ip)
+                break
+        info_agent = ([hostname, status, last_checkin, policy_id])
+        if policy_id not in listAgentOfflineSeperateByPolicy:
+            listAgentOfflineSeperateByPolicy[policy_id] = []
+        if info_agent not in listAgentOfflineSeperateByPolicy[policy_id]:
+            listAgentOfflineSeperateByPolicy[policy_id].append(info_agent)
+    return listAgentOfflineSeperateByPolicy
+
 def main():
     alert = "\t=== SOC Agent Health Check ===\n"
     for i in config.FLEET_SERVERS:
         online, offline = AgentStatus(config.FLEET_SERVERS[i]["FLEET_URL"], config.FLEET_SERVERS[i]["FLEET_PORT"], config.FLEET_SERVERS[i]["FLEET_USERNAME"], config.FLEET_SERVERS[i]["FLEET_PASSWORD"])
-        alert += f">>>>> {config.FLEET_SERVERS[i]['FLEET_URL']} <<<<<\n"
-
+        
+        alert += f">>>> {config.FLEET_SERVERS[i]['FLEET_URL']} <<<<\n"
         if offline == 0:
-            alert += f"Online: {online},\t Offline: {offline} ✅\n"
+            alert += f"Agents online: {online},\t agent offline: {offline}✅\n"
         else:
-            alert += f"Online: {online},\t Offline: {offline} ❌.\t"
-            alert += "List Offline:\n"
+            alert += f"Online: {online}\t Offline: {offline}. List Offline : \n"
 
-        print(f"FLEET_SERVER: {config.FLEET_SERVERS[i]['FLEET_URL']}")
-        print("Online: ", online)
-        print("Offline: ", offline)
+        print("Number of online agents: ", online)
+        print("Number of offline agents: ", offline)
 
-        agents_offline_by_policy = OfflineAgentInfo(config.FLEET_SERVERS[i]["FLEET_URL"], config.FLEET_SERVERS[i]["FLEET_PORT"], config.FLEET_SERVERS[i]["FLEET_USERNAME"], config.FLEET_SERVERS[i]["FLEET_PASSWORD"])
+        offline_agents = OfflineAgentInfo(config.FLEET_SERVERS[i]["FLEET_URL"], config.FLEET_SERVERS[i]["FLEET_PORT"], config.FLEET_SERVERS[i]["FLEET_USERNAME"], config.FLEET_SERVERS[i]["FLEET_PASSWORD"])
+        online_agents = OnlineAgentInfo(config.FLEET_SERVERS[i]["FLEET_URL"], config.FLEET_SERVERS[i]["FLEET_PORT"], config.FLEET_SERVERS[i]["FLEET_USERNAME"], config.FLEET_SERVERS[i]["FLEET_PASSWORD"])
 
-        for i, policy in enumerate(agents_offline_by_policy):
-            alert += f"Policy{i+1}: \t{policy}\n"
+        listAgentOfflineSeperateByPolicy = GetAgentOnlineSeperateByPolicy(offline_agents)
+        listAgentOnlineSeperateByPolicy = GetAgentOnlineSeperateByPolicy(online_agents)
+        alert += "_______________________________\n"
 
-            for i, agent in enumerate(agents_offline_by_policy[policy]):
-                hostname, ips, os, status, last_checkin = agent
-                ip_list = []
-                for ip in ips:
-                    if ip[0:2] == "10" or ip[0:6] == "172.16" or ip[0:7] == "192.168":
-                        ip_list.append(ip)
+        tmp = 0
+        for i, policy in enumerate(listAgentOfflineSeperateByPolicy):
+                    id = policy
+                    id = change(config.FLEET_SERVERS[1]["FLEET_URL"], config.FLEET_SERVERS[1]["FLEET_PORT"], config.FLEET_SERVERS[1]["FLEET_USERNAME"], config.FLEET_SERVERS[1]["FLEET_PASSWORD"], id)
+                    alert += f"\n Policy{i+1}: {id} \n"
+                    if policy not in listAgentOnlineSeperateByPolicy:
+                        alert += f"Onine: 0   "
+                    else:
+                        alert += f"Online: {len(listAgentOnlineSeperateByPolicy[policy])} \t"
+                    alert += f"Offline: { len(listAgentOfflineSeperateByPolicy[policy])} ❌ \n"
+                    alert += f"List ofline:\n"
+                    count=0
+                    for info_agent in listAgentOfflineSeperateByPolicy[policy]:
+                        tmp=0
+                        alert += f"[{count+1}]. "
+                        for detail_info in info_agent:
+                            if (tmp == 1 or tmp == 0):  
+                                alert += f"{detail_info}, \t"
+                            if tmp == 2:
+                                alert += f"LastSeen: {detail_info}\n"                  
+                            tmp = tmp + 1
+                        count = count + 1
+                        if (count >= 3):
+                            break
+                    tmp = i+1
+        
+        for i, policy in enumerate(listAgentOnlineSeperateByPolicy):
+            if policy not in listAgentOfflineSeperateByPolicy:
+                id = policy
+                id = change(config.FLEET_SERVERS[1]["FLEET_URL"], config.FLEET_SERVERS[1]["FLEET_PORT"], config.FLEET_SERVERS[1]["FLEET_USERNAME"], config.FLEET_SERVERS[1]["FLEET_PASSWORD"], id)
+                alert += f"\n Policy{tmp+1}: {id} ✅ \n"
+                tmp+=1
+                alert += f"Online: {len(listAgentOnlineSeperateByPolicy[policy])}   "
+                alert += f"Offline: 0 \n"
                 
-                alert += f"{i + 1}.\t"
-                alert += f"Hostname: {hostname},\t"
-                alert += f"IP: {ip_list},\t"
-                alert += f"OS: {os},\t"
-                alert += f"Status: {status},\t"
-                alert += f"LastSeen: {last_checkin}\n"
-            alert += "_______________________________\n"
 
-    SendAlertToTelegram(config.TELEGRAM_TOKEN, config.TELEGRAM_ID, alert)
+    print(f"\n{alert}")
+    
     SendAlertToTheHive(config.THEHIVE_SERVER, config.THEHIVE_PORT, config.THEHIVE_TOKEN, alert)
+    SendAlertToTelegram(config.TELEGRAM_TOKEN, config.TELEGRAM_ID, alert)
 
 main()
-Footer
